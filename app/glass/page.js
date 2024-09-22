06b9d6d3023442c0c1715d7fa3b49c5f0746412a
Administrator <gitlab_admin_79a569@example.com>
2024-09-22 23:06:28 +0000

    Glassmorphism and Background Animation

---
+++ app/glass/page.js
@@ -0,0 +1,40 @@
+
+
+const page = () => {
+  return (
+    <div className='glass-container'>
+        <div className="bubbles">
+            <span style={{'--i':'11'}}></span>
+            <span style={{'--i':'15'}}></span>
+            <span style={{'--i':'21'}}></span>
+            <span style={{'--i':'14'}}></span>
+            <span style={{'--i':'17'}}></span>
+            <span style={{'--i':'26'}}></span>
+            <span style={{'--i':'13'}}></span>
+            <span style={{'--i':'15'}}></span>
+            <span style={{'--i':'27'}}></span>
+            <span style={{'--i':'14'}}></span>
+            <span style={{'--i':'9'}}></span>
+            <span style={{'--i':'24'}}></span>
+            <span style={{'--i':'11'}}></span>
+            <span style={{'--i':'15'}}></span>
+            <span style={{'--i':'21'}}></span>
+            <span style={{'--i':'14'}}></span>
+            <span style={{'--i':'17'}}></span>
+            <span style={{'--i':'26'}}></span>
+            <span style={{'--i':'13'}}></span>
+            <span style={{'--i':'15'}}></span>
+            <span style={{'--i':'27'}}></span>
+            <span style={{'--i':'14'}}></span>
+            <span style={{'--i':'9'}}></span>
+            <span style={{'--i':'24'}}></span>
+
+
+        </div>
+        <div className='glass'></div>
+        <div className='glass-2'></div> 
+    </div>
+  )
+}
+
+export default page
